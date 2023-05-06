d1de988daacf1a2eb0cf78caef0b11ada08530ae
Zach Wang <wangziqi0325@gmail.com>
2023-05-07 00:38:29 +1200

    feat: add next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@
 import App, {AppProps} from 'next/app';
 import {AppLayout} from "@/components/Layout";
 import '@/styles/globals.scss';
+import {DefaultSeo} from "next-seo";
+import SEO from '../../next-seo.config.json';
 
 export default function MyApp({Component, pageProps}: AppProps) {
-    return <AppLayout>
-        <Component {...pageProps} />
-    </AppLayout>;
+    return (
+        <AppLayout>
+            <DefaultSeo {...SEO} />
+            <Component {...pageProps} />
+        </AppLayout>
+    );
 }
 
 
