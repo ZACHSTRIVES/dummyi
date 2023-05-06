24869f275539bdc6155bc541af21752f7805cec3
thongteav <thong.teav@gmail.com>
2023-05-06 22:12:57 +1200

    Fix: nav items layout

--- src/components/Layout/src/components/NavBar.tsx
+++ src/components/Layout/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
-import {Avatar, Button, Nav} from "@douyinfe/semi-ui";
-import {IconBell, IconHelpCircle, IconTerminal, IconMenu} from "@douyinfe/semi-icons";
-import React, {FunctionComponent, useState} from "react";
+import { Avatar, Button, Nav } from "@douyinfe/semi-ui";
+import { IconBell, IconHelpCircle, IconTerminal, IconMenu } from "@douyinfe/semi-icons";
+import React, { FunctionComponent, useState } from "react";
 
 import styles from './NavBar.module.css';
 
@@ -11,34 +11,34 @@ export const NavBar: FunctionComponent<NavBarProps> = () => {
 
     const handleMenuClick = () => {
         setIsMenuOpen(!isMenuOpen);
+        console.log("is opened:", isMenuOpen);
     };
 
     return (
         <div>
             <Nav
                 mode="horizontal"
-                className="navbar"
+                className={`navbar`}
                 defaultSelectedKeys={["Home"]}
                 onSelect={(key) => console.log(key)}
             >
                 <Nav.Header
-                    logo={<IconTerminal style={{fontSize: 36}}/>}
+                    logo={<IconTerminal style={{ fontSize: 36 }} />}
                     text={"mockdata.co.nz"}
                 />
 
-                <Nav.Item className={styles.navItems} itemKey="Home" text="Home"/>
-                <Nav.Item className={styles.navItems} itemKey="Live" text="Generator"/>
-                <Nav.Item className={styles.navItems} itemKey="Setting" text="About"/>
-
+                <Nav.Item className={styles.navItems} itemKey="Home" text="Home" />
+                <Nav.Item className={styles.navItems} itemKey="Live" text="Generator" />
+                <Nav.Item className={styles.navItems} itemKey="Setting" text="About" />
+                
                 <Nav.Footer>
                     <Button
                         className={styles.hamburgerIcon}
                         onClick={handleMenuClick}
                         theme="borderless"
-                        icon={<IconMenu size="large" color={'black'}/>}
+                        icon={<IconMenu size="large" color={"black"} />}
                     />
                 </Nav.Footer>
-
             </Nav>
         </div>
     );
