33f362f1e90940e90248e893d1cb65fda2a1504f
thongteav <thong.teav@gmail.com>
2023-05-07 12:00:23 +1200

    responsive nav items

--- src/components/Layout/src/components/NavBar.tsx
+++ src/components/Layout/src/components/NavBar.tsx
@@ -11,34 +11,38 @@ export const NavBar: FunctionComponent<NavBarProps> = () => {
 
     const handleMenuClick = () => {
         setIsMenuOpen(!isMenuOpen);
-        console.log("is opened:", isMenuOpen);
+
+        const links = document.querySelector('.semi-navigation-list-wrapper');
+
+        links?.classList.toggle('active');
     };
 
     return (
         <div>
             <Nav
                 mode="horizontal"
-                className={`navbar`}
+                className={styles.navBar}
                 defaultSelectedKeys={["Home"]}
                 onSelect={(key) => console.log(key)}
             >
                 <Nav.Header
                     logo={<IconTerminal style={{ fontSize: 36 }} />}
                     text={"mockdata.co.nz"}
-                />
-
-                <Nav.Item className={styles.navItems} itemKey="Home" text="Home" />
-                <Nav.Item className={styles.navItems} itemKey="Live" text="Generator" />
-                <Nav.Item className={styles.navItems} itemKey="Setting" text="About" />
-                
-                <Nav.Footer>
+                >
                     <Button
                         className={styles.hamburgerIcon}
                         onClick={handleMenuClick}
                         theme="borderless"
                         icon={<IconMenu size="large" color={"black"} />}
                     />
-                </Nav.Footer>
+                </Nav.Header>
+
+                <Nav.Item className={styles.navItems} itemKey="Home" text="Home" />
+                <Nav.Item className={styles.navItems} itemKey="Live" text="Generator" />
+                <Nav.Item className={styles.navItems} itemKey="Setting" text="About" />
+
+                <Nav.Footer></Nav.Footer>
+                
             </Nav>
         </div>
     );
