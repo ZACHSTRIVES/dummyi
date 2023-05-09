9545c409fd42c006b610ad3803011d8fd3995966
Zach Wang <wangziqi0325@gmail.com>
2023-05-09 20:20:44 +1200

    feat: add colormode switch button to nav bar

--- src/components/Layout/src/components/NavBar.tsx
+++ src/components/Layout/src/components/NavBar.tsx
@@ -1,19 +1,22 @@
-import { Avatar, Button, Nav } from "@douyinfe/semi-ui";
-import { IconBell, IconHelpCircle, IconTerminal, IconMenu } from "@douyinfe/semi-icons";
-import React, { FunctionComponent, useState } from "react";
-
+import {Button, Nav} from "@douyinfe/semi-ui";
+import {IconMenu, IconTerminal} from "@douyinfe/semi-icons";
+import React, {FunctionComponent, useState} from "react";
 import styles from './NavBar.module.css';
+import {ColorModeSwitchButton} from "@/components/Layout/src/components/ColorModeSwitchButton";
+import {ColorMode} from "@/constents/enums";
+import {useSelector} from "react-redux";
+import {Store} from "@/types/system";
 
 export type NavBarProps = {}
 
 export const NavBar: FunctionComponent<NavBarProps> = () => {
+
+    const colorMode: ColorMode = useSelector((state: Store) => state.settings.colorMode);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleMenuClick = () => {
         setIsMenuOpen(!isMenuOpen);
-
         const links = document.querySelector('.semi-navigation-list-wrapper');
-
         links?.classList.toggle('active');
     };
 
@@ -26,23 +29,26 @@ export const NavBar: FunctionComponent<NavBarProps> = () => {
                 onSelect={(key) => console.log(key)}
             >
                 <Nav.Header
-                    logo={<IconTerminal style={{ fontSize: 36 }} />}
-                    text={"mockdata.co.nz"}
+                    logo={<IconTerminal
+                        style={{fontSize: 36, color: colorMode === ColorMode.DARK ? 'white' : "black"}}/>}
+                    text={"Mockdata.co.nz"}
                 >
                     <Button
                         className={styles.hamburgerIcon}
                         onClick={handleMenuClick}
                         theme="borderless"
-                        icon={<IconMenu size="large" color={"black"} />}
+                        icon={<IconMenu size="large" style={{color: colorMode === ColorMode.DARK ? 'gray' : "black"}} />}
                     />
                 </Nav.Header>
 
-                <Nav.Item className={styles.navItems} itemKey="Home" text="Home" />
-                <Nav.Item className={styles.navItems} itemKey="Live" text="Generator" />
-                <Nav.Item className={styles.navItems} itemKey="Setting" text="About" />
+                <Nav.Item className={styles.navItems} itemKey="Home" text="Home"/>
+                <Nav.Item className={styles.navItems} itemKey="Live" text="Generator"/>
+                <Nav.Item className={styles.navItems} itemKey="Setting" text="About"/>
 
-                <Nav.Footer></Nav.Footer>
-                
+                <Nav.Footer>
+                    <ColorModeSwitchButton/>
+                </Nav.Footer>
+
             </Nav>
         </div>
     );
