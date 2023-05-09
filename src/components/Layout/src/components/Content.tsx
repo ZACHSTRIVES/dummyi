17310c5121d2a8b4d0e2acd085fa164ef8ddc329
thongteav <thong.teav@gmail.com>
2023-05-09 21:40:33 +1200

    feat: add sticky header and footer

--- src/components/Layout/src/components/Content.tsx
+++ src/components/Layout/src/components/Content.tsx
@@ -2,13 +2,15 @@ import {Breadcrumb, Layout, Skeleton} from "@douyinfe/semi-ui";
 import React, {FunctionComponent} from "react";
 
 export type ContentProps = {
-    children: React.ReactNode
+    children: React.ReactNode,
+    className?: string
 }
 
-export const Content: FunctionComponent<ContentProps> = ({children}) => {
+export const Content: FunctionComponent<ContentProps> = ({children, className}) => {
     const {Content} = Layout;
     return (
         <Content
+            className={className}
             style={{
                 padding: '24px',
                 backgroundColor: 'var(--semi-color-bg-0)',
