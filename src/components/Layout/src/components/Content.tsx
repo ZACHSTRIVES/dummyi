87c51d56749dda1a840a3c6418268c99c6dff251
thongteav <thong.teav@gmail.com>
2023-05-11 21:27:29 +1200

    add reflex-split-pane on workspace

--- src/components/Layout/src/components/Content.tsx
+++ src/components/Layout/src/components/Content.tsx
@@ -20,8 +20,7 @@ export const Content: FunctionComponent<ContentProps> = ({children, className}) 
                 style={{
                     borderRadius: '10px',
                     border: '1px solid var(--semi-color-border)',
-                    height: '100%',
-                    padding: '32px',
+                    height: '100%'
                 }}
             >
                 {children}
--- src/pages/workspace/index.tsx
+++ src/pages/workspace/index.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import InputPane from "@/components/Layout/src/components/InputPane";
+import PreviewPane from "@/components/Layout/src/components/PreviewPane";
+import React, { useEffect, useState } from "react";
+
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex';
+
+import styles from './index.module.css';
 
 export default function Workspace() {
+  const [orientation, setOrientation] = useState(getOrientation());
+
+  useEffect(() => {
+    function handleResize() {
+      const newOrientation = getOrientation();
+      setOrientation(newOrientation);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function getOrientation(): "horizontal" | "vertical" {
+    
+    return window.innerWidth < window.innerHeight ? "horizontal" : "vertical";
+  }
+
   return (
-    <div>
-      <h1>Workspace</h1>
-    </div>
+    <ReflexContainer orientation={orientation}>
+      <ReflexElement>
+        <InputPane/>
+      </ReflexElement>
+
+      <ReflexSplitter className={`${styles.splitter} ${orientation}`} />
+
+      <ReflexElement>
+        <PreviewPane/>
+      </ReflexElement>
+    </ReflexContainer>
   );
 }
