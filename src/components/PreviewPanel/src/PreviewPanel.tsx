93c086670f3d443c62b7d1e6e909fcc6c2627135
Zach Wang <wangziqi0325@gmail.com>
2023-05-13 14:22:09 +1200

    feat: install CodeMirror

--- src/components/PreviewPanel/src/PreviewPanel.tsx
+++ src/components/PreviewPanel/src/PreviewPanel.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {RawPreviewer} from "@/components/PreviewPanel/src/components";
 
 export type PreviewPanelProps = {}
 
 export const PreviewPanel: React.FunctionComponent<PreviewPanelProps> = () => {
     return (
         <div>
-            Preview Pane
+            <RawPreviewer />
         </div>
     );
 };
