43f767c3b504df9cf6f9660bbbc90d8ec6175e85
Zach Wang <wangziqi0325@gmail.com>
2023-05-13 16:46:11 +1200

    feat: codemirror setups

--- src/components/PreviewPanel/src/PreviewPanel.tsx
+++ src/components/PreviewPanel/src/PreviewPanel.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {RawPreviewer} from "@/components/PreviewPanel/src/components";
+import styles from './PreviewPanel.module.css';
+import {ComponentSize} from "@/constents/enums";
 
 export type PreviewPanelProps = {}
 
 export const PreviewPanel: React.FunctionComponent<PreviewPanelProps> = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [panelHeight, setPanelHeight] = React.useState(800);
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver((entries) => {
+            const containerHeight = entries[0].contentRect.height;
+            setPanelHeight(containerHeight);
+        });
+
+        if (containerRef.current) {
+            resizeObserver.observe(containerRef.current);
+        }
+
+        return () => {
+            resizeObserver.disconnect();
+        };
+    }, [containerRef]);
+
     return (
-        <div>
-            <RawPreviewer />
+        <div className={styles.previewPanel} ref={containerRef}>
+            <RawPreviewer height={panelHeight}/>
         </div>
     );
 };
