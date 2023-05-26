569982b4ccee3a8b7bd674c62fa63d43ff4a28e2
thongteav <thong.teav@gmail.com>
2023-05-26 22:14:41 +1200

    add files panel

---
+++ src/components/FilesPanel/src/FilesPanel.tsx
@@ -0,0 +1,43 @@
+import { Tree } from '@douyinfe/semi-ui';
+import React from 'react';
+
+import styles from "./FilesPanel.module.css";
+
+export type TreeNode = {
+  label: string;
+  value: string;
+  key: string;
+  children?: TreeNode[];
+};
+
+export type FilesPanelProps = {
+    files: { label: string; children?: FilesPanelProps['files'] }[];
+};
+
+export const FilesPanel: React.FunctionComponent<FilesPanelProps> = ({ files }) => {
+    const convertToTreeData = (data: FilesPanelProps['files'], parentKey: string): TreeNode[] => {
+        return data.map((node, index) => {
+            const key = parentKey ? `${parentKey}-${index}` : `${index}`;
+            const treeNode: TreeNode = {
+                label: node.label,
+                value: node.label,
+                key: key
+            };
+
+            if (node.children) {
+                treeNode.children = convertToTreeData(node.children, key);
+            }
+
+            return treeNode;
+        });
+    };
+
+    const treeData = convertToTreeData(files, '');
+
+    return (
+        <Tree
+            treeData={treeData}
+            directory
+        />
+    );
+}
--- src/pages/workspace/index.tsx
+++ src/pages/workspace/index.tsx
@@ -1,14 +1,15 @@
-import React, {useEffect} from "react";
-import {ReflexContainer, ReflexElement, ReflexSplitter} from 'react-reflex';
+import React, { useEffect } from "react";
+import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
 import styles from './workspace.module.css';
-import {InputPanel} from "@/components/InputPanel";
-import {PreviewPanel} from "@/components/PreviewPanel";
-import {useDispatch, useSelector} from "react-redux";
-import {Store} from "@/types/system";
-import {ColorMode, PanelsOrientation} from "@/constants/enums";
-import {doSetPanelsOrientation} from "@/reducers/workspace/workspaceActions";
+import { InputPanel } from "@/components/InputPanel";
+import { PreviewPanel } from "@/components/PreviewPanel";
+import { useDispatch, useSelector } from "react-redux";
+import { Store } from "@/types/system";
+import { ColorMode, PanelsOrientation } from "@/constants/enums";
+import { doSetPanelsOrientation } from "@/reducers/workspace/workspaceActions";
 import Head from "next/head";
-import {useIntl} from "@/locale";
+import { useIntl } from "@/locale";
+import { FilesPanel } from "@/components/FilesPanel/src";
 
 export default function Workspace() {
     const intl = useIntl();
@@ -17,6 +18,28 @@ export default function Workspace() {
     // store
     const panelsDirection = useSelector((state: Store) => state.workspace.panelsOrientation);
     const colorMode = useSelector((state: Store) => state.app.colorMode);
+
+    const files = [
+        {
+            label: 'Asia',
+            children: [
+                {
+                    label: 'China',
+                    children: [
+                        {
+                            label: 'Beijing',
+                        },
+                        {
+                            label: 'Guangzhou',
+                        },
+                    ],
+                },
+                {
+                    label: 'Japan',
+                },
+            ],
+        },
+    ];
 
     useEffect(() => {
         setOrientation(getOrientation());
@@ -43,13 +66,26 @@ export default function Workspace() {
     return (
         <>
             <Head>
-                <title>{intl.formatMessage({id:"nav.item.workspace"})} - Duymmi</title>
+                <title>{intl.formatMessage({ id: "nav.item.workspace" })} - Duymmi</title>
             </Head>
 
             <ReflexContainer orientation={panelsDirection}>
+                <ReflexElement minSize={200}>
+                    <FilesPanel files={files} />
+                </ReflexElement>
+
+                <ReflexSplitter
+                    style={{
+                        backgroundColor: colorMode === ColorMode.DARK ? 'rgba(153,153,153,0.44)' : '#d5d3d3',
+                        borderColor: 'transparent',
+                        borderWidth: '1px'
+                    }}
+                    className={`${styles.splitter} vertical`}
+                />
+
                 <ReflexElement minSize={panelsDirection === PanelsOrientation.HORIZONTAL ? 200 : 375}
-                               className={styles.leftReflexElement}>
-                    <InputPanel/>
+                    className={styles.leftReflexElement}>
+                    <InputPanel />
                 </ReflexElement>
 
                 <ReflexSplitter
@@ -62,7 +98,7 @@ export default function Workspace() {
                 />
 
                 <ReflexElement minSize={panelsDirection === PanelsOrientation.HORIZONTAL ? 100 : 400}>
-                    <PreviewPanel/>
+                    <PreviewPanel />
                 </ReflexElement>
             </ReflexContainer>
         </>
