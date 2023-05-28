49aeb70c0f57cc85dc8563b07ae6a36a0e9f8346
Thong Teav <thongteav@users.noreply.github.com>
2023-05-28 13:39:33 +1200

    add draggable and onDrop (#101)

--- src/components/FilesPanel/src/FilesPanel.tsx
+++ src/components/FilesPanel/src/FilesPanel.tsx
@@ -1,13 +1,13 @@
 import { Tree } from '@douyinfe/semi-ui';
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from "./FilesPanel.module.css";
 
 export type TreeNode = {
-  label: string;
-  value: string;
-  key: string;
-  children?: TreeNode[];
+    label: string;
+    value: string;
+    key: string;
+    children?: TreeNode[];
 };
 
 export type FilesPanelProps = {
@@ -32,12 +32,71 @@ export const FilesPanel: React.FunctionComponent<FilesPanelProps> = ({ files }) 
         });
     };
 
-    const treeData = convertToTreeData(files, '');
+    const initTreeData = convertToTreeData(files, '');
+
+    const [treeData, setTreeData] = useState(initTreeData);
+
+    const onDrop = (info) => {
+        const { dropToGap, node, dragNode } = info;
+        const dropKey = node.key;
+        const dragKey = dragNode.key;
+        const dropPos = node.pos.split('-');
+        const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
+
+        const data = [...treeData];
+        const loop = (data, key, callback) => {
+            data.forEach((item, index, arr) => {
+                if (item.key === key) return callback(item, index, arr);
+                if (item.children) return loop(item.children, key, callback);
+            });
+        };
+
+        let dragObj;
+        loop(data, dragKey, (item, index, arr) => {
+            arr.splice(index, 1);
+            dragObj = item;
+        });
+
+        if (!dropToGap) {
+            // inset into the dropPosition
+            loop(data, dropKey, (item, ind, arr) => {
+                item.children = item.children || [];
+                item.children.push(dragObj);
+            });
+        }
+        else if (dropPosition === 1 && node.children && node.expanded) {
+            // has children && expanded and drop into the node bottom gap
+            // could insert anywhere. Here we insert to the top.
+            loop(data, dropKey, item => {
+                item.children = item.children || [];
+                item.children.unshift(dragObj);
+            });
+        }
+        else {
+            let dropNodeInd;
+            let dropNodePosArr;
+            loop(data, dropKey, (item, ind, arr) => {
+                dropNodePosArr = arr;
+                dropNodeInd = ind;
+            });
+            if (dropPosition === -1) {
+                // insert to top
+                dropNodePosArr.splice(dropNodeInd, 0, dragObj);
+            }
+            else {
+                // insert to bottom
+                dropNodePosArr.splice(dropNodeInd + 1, 0, dragObj);
+            }
+        }
+        setTreeData(data);
+    }
 
     return (
         <Tree
             treeData={treeData}
             directory
+            draggable
+            onDrop={onDrop}
         />
     );
 }
