347bf8f210c428338ee643274ce3e3e93dbefb5d
thongteav <thong.teav@gmail.com>
2023-06-03 23:11:17 +1200

    unused references

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,5 @@
-import React, { ReactElement } from "react";
-import { NextPageWithLayout } from "../_app";
-import { useRouter } from "next/router";
+import React from "react";
 import { AboutLayout } from "@/components/Layout/about";
-import DatePage from "./date";
-import NumberPage from "./number";
-import StringPage from "./string";
 
 
 const About: React.FC = () => {
