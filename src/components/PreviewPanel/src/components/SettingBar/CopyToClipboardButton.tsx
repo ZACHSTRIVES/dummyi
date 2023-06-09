06c3b0b6da0df855a3ddfd35c09c7623d12a318e
Zach Wang <wangziqi0325@gmail.com>
2023-06-09 12:18:39 +1200

    feat: add copy to clipboard function for preview table (#138)

--- src/components/PreviewPanel/src/components/SettingBar/CopyToClipboardButton.tsx
+++ src/components/PreviewPanel/src/components/SettingBar/CopyToClipboardButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Button, Toast, Tooltip} from "@douyinfe/semi-ui";
 import {IconCopy} from "@douyinfe/semi-icons";
-import {useIntl} from "@/locale";
+import {FormattedMessage, useIntl} from "@/locale";
 import {useSelector} from "react-redux";
 import {selectPreviewFormattedData} from "@/reducers/workspace/workspaceSelectors";
 
@@ -15,20 +15,16 @@ export const CopyToClipboardButton: React.FunctionComponent<CopyToClipboardButto
 
     // actions
     const copyToClipboard = () => {
-        const input = document.createElement('textarea');
-        input.value = previewFormattedData;
-        document.body.appendChild(input);
-        input.select();
-        document.execCommand('copy');
-        document.body.removeChild(input);
-        Toast.success(intl.formatMessage({id: "preview.setting.copyToClipboard.notification.content"}));
+        navigator.clipboard.writeText(previewFormattedData).then(() => {
+            Toast.success(intl.formatMessage({id: "preview.setting.copyToClipboard.notification.content"}));
+        });
     }
 
     return (
         <Tooltip
             position={'bottom'}
             trigger={'hover'}
-            content={intl.formatMessage({id: "preview.setting.copyToClipboard.tooltip"})}
+            content={<FormattedMessage id={"preview.setting.copyToClipboard.tooltip"}/>}
         >
             <Button
                 onClick={copyToClipboard}
