eff5477f7eaed666087ccc920c8038c4a823f714
Zach Wang <wangziqi0325@gmail.com>
2024-03-12 00:40:46 +1300

    local: update jaJP

--- src/locale/translations/jaJP.ts
+++ src/locale/translations/jaJP.ts
@@ -1,106 +1,200 @@
-// Translated by ChatGPT
 export const jaJP = {
-
     // -------------------------------------------------------------------------------------------------------------
     // export
 
     // export category
-    "export.category.fileTypes": "一般的なファイルタイプ",
-    "export.category.databases" : "Databases",
-    "export.category.programmingLanguages": "プログラミング言語",
+    "export.category.file_types": "一般的な形式",
+    "export.category.databases": "データベース",
+    "export.category.programming_languages": "プログラミング言語",
 
     // export format modal
-    "export.configurator.modal.title": "エクスポート形式",
-    "export.configurator.modal.closeButton.text": "閉じる",
+    "export.configurator.modal.title": "形式を生成",
+    "export.configurator.modal.confirmButton.text": "確認",
     "export.configurator.config.label": "設定",
-    "export.configurator.config.empty": "現在、このエクスポート形式の設定は利用できません。",
+    "export.configurator.config.empty": "この出力形式の設定は現在利用できません",
+
+    // export modal
+    "export.modal.title": "データを一括生成",
+    "export.modal.exportNumOfRows.label": "生成するデータ行数",
+    "export.modal.exportNumOfRows.empty": "データ行数を空にすることはできません",
+    "export.modal.exportFormat.label": "生成形式",
+    "export.modal.estimatedSize.label": "推定サイズ",
+    "export.modal.estimatedTime.label": "推定時間",
+    "export.modal.exportFileName.label": "ファイル名",
+    "export.modal.exportFileName.empty": "ファイル名は空にできません",
+    "export.modal.cancel.button.text": "キャンセル",
+    "export.modal.generate.button.text": "生成",
+    "export.modal.hide.button.text": "隠す",
+    "export.modal.terminate.button.text": "終了",
+    "export.modal.generating.rows.text": "行数",
+    "export.modal.generating.time.text": "時間",
+    "export.modal.generating.done.text": "完了",
+    "export.modal.toast.details.button.text": "詳細",
+    "export.modal.toast.download.button.text": "ダウンロード",
 
     // csv
     "export.configurator.csv.delimiter": "区切り文字",
-    "export.configurator.csv.includeHeader": "ヘッダーを含める",
-    "export.configurator.csv.endLineChar": "行末文字",
+    "export.configurator.csv.delimiter.required": "区切り文字を空にすることはできません",
+    "export.configurator.csv.includeHeader": "ヘッダを含む",
+    "export.configurator.csv.endLineChar": "行終了文字",
+
+    // xml
+    "export.configurator.xml.rootNodeName": "ルートノード",
+    "export.configurator.xml.childNodeName": "子ノード",
+    "export.configurator.xml.encoding": "エンコーディング",
+    "export.configurator.xml.indentSize": "インデントのサイズ",
+
+    // json
+    "export.configurator.json.insideArray": "配列内",
+    "export.configurator.json.includeNullValues": "null値を含む",
+
+    // javascript
+    "export.configurator.javascript.format": "形式",
+    "export.configurator.javascript.format.variable": "変数",
+    "export.configurator.javascript.format.export": "エクスポート",
+    "export.configurator.javascript.varName": "変数名",
+    "export.configurator.javascript.varName.required": "変数名を空にすることはできません",
+    "export.configurator.javascript.declarationKeyword": "宣言タイプ",
+    "export.configurator.javascript.module": "モジュール",
+
+    // sql
+    "export.configurator.sql.type": "データベースのタイプ",
+    "export.configurator.sql.tableName": "テーブル名",
+    "export.configurator.sql.tableName.required": "テーブル名を空にすることはできません",
+    "export.configurator.sql.statement": "SQLステートメント",
+    "export.configurator.sql.batchSize": "バッチサイズ",
+    "export.configurator.sql.includeDropTable": "`DROP TABLE` を含む",
+    "export.configurator.sql.includeCreateTable": "`CREATE TABLE` を含む",
+    "export.configurator.sql.includePrimaryKey": "主キーを含む",
+    "export.configurator.sql.primaryKeyColumnName": "主キーカラム名",
+
+    // c#
+    "export.configurator.csharp.collectionType": "コレクションタイプ",
+    "export.configurator.csharp.collectionName": "コレクションの変数名",
+    "export.configurator.csharp.dtoClass": "DTOクラスを作成",
+    "export.configurator.csharp.dtoClassName": "クラス名",
+
+    // typescript
+
+
+    "export.configurator.typescript.declarationType": "宣言のタイプ",
+    "export.configurator.typescript.declarationType.interface.name": "Interfaceの名前",
+    "export.configurator.typescript.declarationType.type.name": "Typeの名前",
+    "export.configurator.typescript.variableName": "変数名",
 
     // -------------------------------------------------------------------------------------------------------------
     // data types
-    
-    
+
+
     // url
-    "dataType.url": "Url",
+    "dataType.url": "URL",
+    "dataType.url.appendSlash.label": "スラッシュ",
+    "dataType.url.protocol.label": "プロトコル",
 
     // domainsuffix
-    "dataType.domainsuffix": "DomainSuffix",
+    "dataType.domainsuffix": "ドメインサフィックス",
 
     // domainname
-    "dataType.domainname": "DomainName",
+    "dataType.domainname": "ドメイン名",
 
     // accountnumber
-    "dataType.accountnumber": "AccountNumber",
+    "dataType.accountnumber": "銀行口座番号",
+    "dataType.accountnumber.length": "長さ",
+    "dataType.accountnumber.empty": "長さは空にできません",
 
     // accountname
-    "dataType.accountname": "AccountName",
+    "dataType.accountname": "アカウント名",
 
     // color
-    "dataType.color": "Color",
+    "dataType.color": "色",
+    "dataType.color.kind.label": "タイプ",
+    "dataType.color.format.humanWord": "言葉での形式",
+    "dataType.color.format.label": "形式",
 
     // phone
-    "dataType.phone": "Phone Number",
-    "dataType.phone.formats.label": "Formats",
-    "dataType.phone.formats.tooltips": "Phone number format, please use \"#\" to represent digits, press Enter key to confirm",
+    "dataType.phone": "電話番号",
+    "dataType.phone.formats.label": "形式",
+    "dataType.phone.formats.tooltips": "電話番号の形式は、「#」を数字の代わりに使用してください。エンターキーで確定",
 
     // emoji
-    "dataType.emoji": "Emoji",
-    "dataType.emoji.type": "Type",
-    "dataType.emoji.type.all": "All",
-    "dataType.emoji.type.smiley": "Smiley",
-    "dataType.emoji.type.body" : "Body",
-    "dataType.emoji.type.person" : "Person",
-    "dataType.emoji.type.nature" : "Nature",
-    "dataType.emoji.type.food" : "Food",
-    "dataType.emoji.type.travel" : "Travel",
-    "dataType.emoji.type.activity" : "Activity",
-    "dataType.emoji.type.object" : "Object",
-    "dataType.emoji.type.symbol" : "Symbol",
-    "dataType.emoji.type.flag" : "Flag",
+    "dataType.emoji": "絵文字",
+    "dataType.emoji.type": "タイプ",
+    "dataType.emoji.type.all": "全て",
+    "dataType.emoji.type.smiley": "スマイリー",
+    "dataType.emoji.type.body": "ボディ",
+    "dataType.emoji.type.person": "人",
+    "dataType.emoji.type.nature": "自然",
+    "dataType.emoji.type.food": "食べ物",
+    "dataType.emoji.type.travel": "旅行",
+    "dataType.emoji.type.activity": "アクティビティ",
+    "dataType.emoji.type.object": "オブジェクト",
+    "dataType.emoji.type.symbol": "シンボル",
+    "dataType.emoji.type.flag": "フラグ",
 
     // persontitle
-    "dataType.persontitle": "PersonTitle",
+    "dataType.persontitle": "称号",
 
     // middlename
-    "dataType.middlename": "MiddleName",
+    "dataType.middlename": "ミドルネーム",
 
     // lastname
-    "dataType.lastname": "LastName",
+    "dataType.lastname": "姓",
 
     // firstname
-    "dataType.firstname": "FirstName",
+    "dataType.firstname": "名",
 
     // sex
-    "dataType.sex": "Sex",
-
-    // data type category
-    "dataType.category.all": "全て",
-    "dataType.category.basic": "基本",
-    "dataType.category.person": "個人",
-    "dataType.category.commerce": "商業",
+    "dataType.sex": "性別",
 
     // number
-    "dataType.number": "数値",
+    "dataType.number": "数字",
     "dataType.number.kind.label": "種類",
     "dataType.number.precision.label": "精度",
     "dataType.number.min.label": "最小値",
+    "dataType.number.min.tooltip": "生成するデータの最小値",
+    "dataType.number.min.errorMessage.empty": "最小値は空にできません",
+    "dataType.number.min.errorMessage.greaterThanMax": "最小値は最大値より大きくできません",
     "dataType.number.max.label": "最大値",
+    "dataType.number.max.tooltip": "生成するデータの最大値",
+    "dataType.number.max.errorMessage.empty": "最大値は空にできません",
+    "dataType.number.max.errorMessage.lessThanMin": "最大値は最小値より小さくできません",
 
     // dateTime
     "dataType.dateTime": "日時",
 
     // boolean
     "dataType.boolean": "ブール値",
+    "dataType.boolean.true.label": "真の確率",
+    "dataType.boolean.true.tooltip": "真(true)を生成する確率",
+    "dataType.boolean.true.errorMessage.empty": "真の確率を空にすることはできません",
+    "dataType.boolean.format.label": "形式",
 
-    // person name
-    "dataType.personName": "氏名",
+    // full name
+    "dataType.fullName": "フルネーム",
+    "dataType.fullName.sex.label": "性別",
+    "dataType.fullName.sex.selectOptions.all": "男性、女性",
+    "dataType.fullName.sex.selectOptions.male": "男性",
+    "dataType.fullName.sex.selectOptions.female": "女性",
+    "dataType.fullName.firstName.label": "名",
+    "dataType.fullName.lastName.label": "姓",
 
-    // account mi,ner
-    "dataType.accountNumber": "口座番号",
+    //email
+    "dataType.email": "メールアドレス",
+    "dataType.email.provider.label": "メールプロバイダー",
+    "dataType.email.allowSpecialCharacters.label": "特殊文字を許可",
+
+    // company name
+    "dataType.companyName": "会社名",
+
+    // account number
+    "dataType.accountNumber": "アカウント番号",
+
+    // data type category
+    "dataType.category.all": "全て",
+    "dataType.category.basic": "基本",
+    "dataType.category.person": "人物",
+    "dataType.category.commerce": "商業",
+    "dataType.category.network": "ネットワーク",
 
     // -------------------------------------------------------------------------------------------------------------
     // pages
@@ -108,45 +202,52 @@ export const jaJP = {
     // nav bar
     "nav.item.home": "ホーム",
     "nav.item.workspace": "ワークスペース",
-    "nav.item.about": "について",
+    "nav.item.about": "紹介",
     "nav.colorModeSwitchButton.switchToDarkMode.text": "ダークモードに切り替え",
     "nav.colorModeSwitchButton.switchToLightMode.text": "ライトモードに切り替え",
     "nav.languageSwitchButton.tooltip": "言語",
-    "nav.languageSwitchModal.title": "言語の選択",
-    "nav.languageSwitchModal.footer.chatGPT.text": "多言語翻訳はChatGPTによって生成されました",
+    "nav.languageSwitchModal.title": "言語を選択",
+    "nav.languageSwitchModal.footer.chatGPT.text": "多言語翻訳はChatGPTによって生成されます",
 
     // workspace
     "toolbar.numOfRowInput.suffix": "行",
-    "toolbar.generateButton.text": "批量生成",
-    "toolbar.panelsOrientationButton.tooltip.switchToColumn": "パネルを列に切り替え",
-    "toolbar.panelsOrientationButton.tooltip.switchToRow": "パネルを行に切り替え",
-    "toolbar.emptyPageButton.tooltip": "ワークスペースを空にする",
-    "toolbar.emptyPageButton.confirmation.title": "ワークスペースを空にする",
-    "toolbar.emptyPageButton.confirmation.text": "本当にワークスペースを空にしますか？設定したワークスペースの構成が失われます。",
-    "toolbar.exportSchemaButton.tooltip": "スキーマのエクスポート",
-    "toolbar.importSchemaButton.tooltip": "スキーマのインポート",
+    "toolbar.generateButton.text": "一括生成",
+    "toolbar.panelsOrientationButton.tooltip.switchToColumn": "パネルを横向きに切り替え",
+    "toolbar.panelsOrientationButton.tooltip.switchToRow": "パネルを縦向きに切り替え",
+    "toolbar.emptyPageButton.tooltip": "ワークスペースをクリア",
+    "toolbar.emptyPageButton.confirmation.title": "ワークスペースをクリアしますか？",
+    "toolbar.emptyPageButton.confirmation.text": "ワークスペースをクリアしますか？すべての設定が削除されます。",
+    "toolbar.exportSchemaButton.tooltip": "スキーマをエクスポート",
+    "toolbar.importSchemaButton.tooltip": "スキーマをインポート",
     "preview.setting.regenerateButton.tooltip": "再生成",
     "preview.setting.lineNumberSwitch.tooltip.show": "行番号を表示",
     "preview.setting.lineNumberSwitch.tooltip.hide": "行番号を非表示",
-    "preview.setting.lineWarpSwitch.tooltip.enable": "行の折り返しを有効にする",
-    "preview.setting.lineWarpSwitch.tooltip.disable": "行の折り返しを無効にする",
+    "preview.setting.lineWarpSwitch.tooltip.enable": "自動改行を有効にする",
+    "preview.setting.lineWarpSwitch.tooltip.disable": "自動改行を無効にする",
     "preview.setting.copyToClipboard.tooltip": "クリップボードにコピー",
     "preview.setting.copyToClipboard.notification.success": "成功",
-    "preview.setting.copyToClipboard.notification.content": "内容がクリップボードにコピーされました。",
-    "preview.setting.rawView.text": "生データ",
-    "preview.setting.tableView.text": "表形式",
-    "dataFields.list.addNewFieldButton.text": "フィールドを追加",
+    "preview.setting.copyToClipboard.notification.content": "クリップボードにコピーされました",
+    "preview.setting.rawView.text": "原文",
+    "preview.setting.tableView.text": "テーブル",
+    "dataFields.list.addNewFieldButton.text": "新しいフィールドを追加",
+    "dataFields.list.noDataFields.text": "フィールドがまだありません",
+    "dataFields.list.createFirstField.text": "最初のフィールドを作成しましょう！",
     "dataFields.input.fieldName.label": "フィールド名",
-    "dataFields.input.type.label": "タイプ",
-    "dataFields.input.type.placeholder": "タイプを選択",
-    "dataFields.input.emptyRate.label": "空の確率",
+    "dataFields.input.fieldName.errorMessage.empty": "フィールド名は空にできません",
+    "dataFields.input.type.label": "データタイプ",
+    "dataFields.input.type.placeholder": "タイプを選択...",
+    "dataFields.input.emptyRate.label": "空値の確率",
+    "dataFields.input.emptyRate.tooltip": "空値(null)を生成する確率",
+    "dataFields.input.emptyRate.errorMessage.empty": "空値の確率は空にできません",
     "dataFields.input.options.label": "オプション",
-    "dataFields.type.modal.title": "データタイプ",
-    "dataFields.type.modal.search.placeholder": "データタイプを検索...",
+    "dataFields.type.modal.title": "タイプを選択",
+    "dataFields.type.modal.search.placeholder": "タイプを検索...",
 
     // error pages
+    "error.input.isRequired": "この項目は必須です",
     "error.404.description": "ページが存在しません",
     "error.404.button.text": "ホームページ",
-    "error.general.description": "おっと！エラーが発生しました。",
+    "error.general.description": "おっと！エラーが発生しました！",
     "error.general.button.text": "ホームページ",
-};
+
+}
