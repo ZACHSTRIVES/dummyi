73bfcda1ddc2e8021653fb06834b3c0020be1973
Zach Wang <wangziqi0325@gmail.com>
2024-03-16 11:02:03 +1300

    fix: mac address generator issue

--- src/core/generators/MacAddress/MacAddress.tsx
+++ src/core/generators/MacAddress/MacAddress.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { GenerateResult, GeneratorOptionsComponentInterface } from "@/types/generator";
-import { faker } from "@faker-js/faker";
-import { OptionsSelect, SelectOption } from "@/components/Utils";
-import { FormattedMessage } from "@/locale";
+import {GenerateResult, GeneratorOptionsComponentInterface} from "@/types/generator";
+import {faker} from "@faker-js/faker";
+import {OptionsSelect, SelectOption} from "@/components/Utils";
+import {FormattedMessage} from "@/locale";
 // -------------------------------------------------------------------------------------------------------------
 // types
 
@@ -11,6 +11,7 @@ export enum MacAddressGeneratorSeparator {
     HYPEN_FORMAT = '-',
     NONE_FORMAT = ''
 }
+
 export interface MacAddressGeneratorOptions {
 
     separator: MacAddressGeneratorSeparator;//separator
@@ -38,23 +39,21 @@ export const generate = (options: MacAddressGeneratorOptions): GenerateResult =>
 
 // -------------------------------------------------------------------------------------------------------------
 // options component
-export const MacAddressGeneratorOptionsComponent: React.FunctionComponent<GeneratorOptionsComponentInterface> = ({ ...props }) => {
-    const { options, handleOptionValueChange } = props as {
+export const MacAddressGeneratorOptionsComponent: React.FunctionComponent<GeneratorOptionsComponentInterface> = ({...props}) => {
+    const {options, handleOptionValueChange} = props as {
         options: MacAddressGeneratorOptions,
         handleOptionValueChange: typeof props.handleOptionValueChange
     };
 
 
-
-
     return (
         <>
             <OptionsSelect
-                label={<FormattedMessage id='dataType.macaddress.format' />}
+                label={<FormattedMessage id='dataType.macaddress.format'/>}
                 selectOptions={SeparatorFormatSelectOptions}
                 value={options.separator}
-                onChange={(v) => handleOptionValueChange("format", v)}
-                style={{ width: '80px' }}
+                onChange={(v) => handleOptionValueChange("separator", v)}
+                style={{width: '80px'}}
             />
         </>
     );
